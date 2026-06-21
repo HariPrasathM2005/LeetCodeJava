b7965e9b2f92d8ced689df467243a113a888f559
HariPrasathM2005 <mo.hariprasath@gmail.com>
2026-06-21 15:37:11 +0530

    Implement toLowerCase method in ToLowerCase709

---
+++ To Lower Case 709.java
@@ -0,0 +1,15 @@
+public class ToLowerCase709 
+{
+    public static String toLowerCase(String s)
+    {
+        String res="";
+        res=s.toLowerCase();
+        return res;
+    }
+    public static void main(String[] args) 
+    {
+        String s="Hello";  
+        String res=toLowerCase(s);
+        System.out.println(res);  
+    }
+}
