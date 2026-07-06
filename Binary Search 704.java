4c2d61c9db8bc984f5b4fb854992266e5e329662
HariPrasathM2005 <mo.hariprasath@gmail.com>
2026-07-06 17:48:13 +0530

    Add Binary Search implementation in BinarySearch704.java
    Implemented a linear search method to find the target in an array.

---
+++ Binary Search 704.java
@@ -0,0 +1,17 @@
+public class BinarySearch704 {
+    public static int search(int[] nums, int target) 
+    {
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==target)
+                return i;
+        }
+        return -1;
+    }
+    public static void main(String[] args) 
+    {
+        int[] nums={-1,0,3,5,9,12};
+        int Res=search(nums,10);
+        System.out.println(Res);
+    }
+}
