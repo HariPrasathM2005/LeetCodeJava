3af331e7e123681e37975377ca7adf0c9ca24000
HariPrasathM2005 <mo.hariprasath@gmail.com>
2026-08-12 20:20:51 +0530

    Implement method to determine day of the week

---
+++ Day of the week 1185.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Solution {
+    public String dayOfTheWeek(int day, int month, int year) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(year,month-1,day);
+        int d = cal.get(Calendar.DAY_OF_WEEK);
+        String[] days={"Sunday", "Monday", "Tuesday","Wednesday", "Thursday", "Friday", "Saturday"};
+        return days[d-1];
+    }
+}
